13898186ada4914a3595ae0641caca52df17ef5b
lkodovski <l.kodovski@hotmail.com>
2025-03-06 09:49:57 +0100

    UpdateDelete

--- ApiMovies/Controllers/CategoriesController.cs
+++ ApiMovies/Controllers/CategoriesController.cs
@@ -79,5 +79,64 @@ namespace ApiMovies.Controllers
             }
             return CreatedAtRoute("GetCategory", new { categoryId = category.Id }, category);
         }
+
+        [HttpPut("{categoryId:int}", Name ="UpdatePutCategory")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        
+        public IActionResult UpdatePutCategory(int categoryId, [FromBody] CategoryDto categoryDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (categoryDto == null || categoryId != categoryDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingCategory = _ctRepo.GetCategory(categoryId);
+            if (existingCategory == null)
+            {
+                return NotFound($"Category with id {categoryId} not found");
+            }
+
+            var category = _mapper.Map<Category>(categoryDto);
+
+            if (!_ctRepo.UpdateCategory(category))
+            {
+                ModelState.AddModelError("", $"Something went wrong with updating category {category.Name}");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{categoryId:int}", Name = "DeleteCategory")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public IActionResult DeleteCategory(int categoryId)
+        {
+            if(!_ctRepo.ExistCategory(categoryId))
+            {
+                return NotFound();
+            }
+
+            var category = _ctRepo.GetCategory(categoryId);
+
+            if (!_ctRepo.DeleteCategory(category))
+            {
+                ModelState.AddModelError("", $"Something went wrong with deleting category {category.Name}");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
     }
 }
